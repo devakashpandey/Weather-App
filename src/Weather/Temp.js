75e6e51a3ffca78df3bf54c8bdd1df971b3c5503
DevAkky <developerakky@gmaiil.com>
2023-01-17 13:27:02 +0530

    adding style

--- src/Weather/Temp.js
+++ src/Weather/Temp.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import "./Temp.css"
 
 function Temp() {
   return (
      <>
-     <h1>WEATHER APP</h1>
+      <div className='wrap'>
+        <div className='search'>
+           <input type="search"
+           placeholder='Enter any place'
+           autoFocus
+           className='searchTerm'
+           /> 
+            <button className='searchButton'>Search</button>
+        </div>
+      </div>
+
+    {/* our tempreture card  */}
+
+    <div className='weather-box'>
+          <div className='weather-icon'>
+          <i className={"wi wi-day-sunny"}></i>
+          </div>
+
+          <div className='weatherInfo'>
+            <div className='tempreture'>
+              <span>25.5&deg;</span>
+            </div>
+          </div>
+    
+
+    </div>
+
+
      </>
   )
 }
