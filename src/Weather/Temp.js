764e15857f9ebad1f789d323d214b808deea617c
DevAkky <developerakky@gmaiil.com>
2023-01-17 19:14:26 +0530

    final style complete

--- src/Weather/Temp.js
+++ src/Weather/Temp.js
@@ -31,14 +31,45 @@ function Temp() {
              <div className='weather-cond'>Sunny</div>
              <div className='place'> Pune,India </div>
           </div>
-
         </div>
 
       <div className='date'>{new Date().toLocaleString()}</div>
 
+      <div className='temp-info'>
+           <div className='temp-min-max'>
+                      <div className='two-sided-section'>
+                         <p><i className={"wi wi-sunset"} ></i></p>
+                         <p className='text-info'>19:19 pm <br/>
+                              Sunset
+                         </p>
+                      </div>
+
+                      <div className='two-sided-section'>
+                         <p><i className={"wi wi-humidity"} ></i></p>
+                         <p className='text-info'>19:19 pm <br/>
+                              Humidity
+                         </p>
+                      </div>
+           </div>
+
+            <div className='temp-min-max'>
+                      <div className='two-sided-section'>
+                         <p><i className={"wi wi-rain"} ></i></p>
+                         <p className='text-info'>19:19 pm <br/>
+                              Rain
+                         </p>
+                      </div>
+
+                      <div className='two-sided-section'>
+                         <p><i className={"wi wi-strong-wind"} ></i></p>
+                         <p className='text-info'>19:19 pm <br/>
+                              Wind
+                         </p>
+                      </div>
+            </div>
+      </div>
+      
     </div>
-
-
 
      </>
   )
