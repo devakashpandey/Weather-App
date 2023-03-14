42902f7bc7166176b82312fcad067bf3bc908d52
DevAkky <developerakky@gmaiil.com>
2023-03-14 09:59:15 +0530

    responsivness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Temp from './Weather/Temp';
+import React from "react";
+import Temp from "./Weather/Temp";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-      <>
-       <Temp/>
-      </>
-  )
+    <>
+      <center>
+        <h1
+          style={{
+            marginBottom: 180,
+            marginTop: -80,
+            color: "white",
+            fontFamily: "sans-serif",
+          }}
+        >
+          WEATHER CHECKER APP
+        </h1>
+      </center>
+      <Temp />
+    </>
+  );
 }
 
 export default App;
--- src/Weather/WeatherCard.js
+++ src/Weather/WeatherCard.js
@@ -1,107 +1,124 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const WeatherCard = ({weatherInfo}) => {
+const WeatherCard = ({ weatherInfo }) => {
+  let [weatherState, setWeatherState] = useState("");
 
-   let [weatherState, setWeatherState] = useState("");
+  let {
+    temp,
+    pressure,
+    humidity,
+    weathermood,
+    speed,
+    country,
+    sunset,
+    cityname,
+  } = weatherInfo;
 
-    let { 
-           temp,
-           pressure,
-           humidity,
-           weathermood,
-           speed,
-           country,
-           sunset,
-           cityname
- } = weatherInfo;
+  useEffect(() => {
+    if (weathermood) {
+      switch (weathermood) {
+        case "Clouds":
+          setWeatherState("wi-day-cloudy");
+          break;
+        case "Clear":
+          setWeatherState("wi-day-sunny");
+          break;
+        case "Smoke":
+          setWeatherState("wi-smoke");
+          break;
+        case "Haze":
+          setWeatherState("wi-fog");
+          break;
+        case "Rain":
+          setWeatherState("wi-day-rain");
+          break;
 
- useEffect(()=>{
-  
-  if(weathermood){
-     switch(weathermood){
+        default:
+          setWeatherState("wi-day-sunny");
+          break;
+      }
+    }
+  }, [weathermood]);
 
-         case "Clouds" : setWeatherState("wi-day-cloudy")
-         break;
-         case "Clear" : setWeatherState("wi-day-sunny")
-         break;
-         case "Smoke" : setWeatherState("wi-smoke")
-         break;
-         case "Haze" : setWeatherState("wi-fog")
-         break;
-         case "Rain" : setWeatherState("wi-day-rain")
-         break;
+  // converting seconds into time ---->>
 
-         default:setWeatherState("wi-day-sunny")
-         break;
-     }
-  }
-    
- },[weathermood])
-
- // converting seconds into time ---->>
-
- let sec = sunset;
- let date = new Date(sec * 1000)
- let time = `${date.getHours()}:${date.getMinutes()} `
-
+  let sec = sunset;
+  let date = new Date(sec * 1000);
+  let time = `${date.getHours()}:${date.getMinutes()} `;
 
   return (
-     <>
-       <div className='weather-box'>
-          <div className='weather-icon'>
-          <i className={`wi ${weatherState}`}></i>
+    <>
+      <div className="weather-box">
+        <div className="weather-icon">
+          <i className={`wi ${weatherState}`} style={{ marginTop: 10 }}></i>
+        </div>
+
+        <div className="weather-info">
+          <div className="temp">
+            <span>{temp}&deg;</span>
           </div>
 
-          <div className='weather-info'>
-            <div className='temp'>
-              <span>{temp}&deg;</span>
+          <div className="description">
+            <div className="weather-cond">{weathermood}</div>
+            <div className="place">
+              {cityname}, {country}
             </div>
-
-          <div className='description'>
-             <div className='weather-cond'>{weathermood}</div>
-             <div className='place'>{cityname}, {country}</div>
           </div>
         </div>
 
-      <div className='date'>{new Date().toLocaleString()}</div>
+        <div className="date">{new Date().toLocaleString()}</div>
 
-      <div className='temp-info'>
-           <div className='temp-min-max'>
-                      <div className='two-sided-section'>
-                         <p><i className={"wi wi-sunset"} ></i></p>
-                         <p className='text-info'>{time}PM<br/>
-                              Sunset
-                         </p>
-                      </div>
-
-                      <div className='two-sided-section'>
-                         <p><i className={"wi wi-humidity"} ></i></p>
-                         <p className='text-info'>{humidity} <br/>
-                              Humidity
-                         </p>
-                      </div>
-           </div>
-
-            <div className='temp-min-max'>
-                      <div className='two-sided-section'>
-                         <p><i className={"wi wi-rain"} ></i></p>
-                         <p className='text-info'>{pressure}<br/>
-                              Pressure
-                         </p>
-                      </div>
-
-                      <div className='two-sided-section'>
-                         <p><i className={"wi wi-strong-wind"} ></i></p>
-                         <p className='text-info'>{speed}<br/>
-                              Speed
-                         </p>
-                      </div>
+        <div className="temp-info">
+          <div className="temp-min-max">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="text-info">
+                {time}PM
+                <br />
+                Sunset
+              </p>
             </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="text-info">
+                {humidity} <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+
+          <div className="temp-min-max">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="text-info">
+                {pressure}
+                <br />
+                Pressure
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="text-info">
+                {speed}
+                <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+    </>
+  );
+};
 
-    </div>
-     </>
-  )
-}
-
-export default WeatherCard
+export default WeatherCard;
